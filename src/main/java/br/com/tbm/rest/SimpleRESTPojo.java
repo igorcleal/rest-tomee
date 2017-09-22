b8287fe332b4399e20e1e26cc2bd772ec4d4121e
igor leal <igorcleal@gmail.com>
2017-09-22 13:51:06 -0300

    inicio projeto

---
+++ src/main/java/br/com/tbm/rest/SimpleRESTPojo.java
@@ -0,0 +1,14 @@
+package br.com.tbm.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.Date;
+
+@Path("/pojo")
+public class SimpleRESTPojo {
+
+    @GET
+    public String pojo() {
+        return "pojo ok @ " + new Date().toString();
+    }
+}
