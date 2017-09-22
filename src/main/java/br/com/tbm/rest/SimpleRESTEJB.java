bdfc0575539e3c2e8d78b954853dc3e782eb5b78
igor leal <igorcleal@gmail.com>
2017-09-22 14:58:06 -0300

    Commit

--- src/main/java/br/com/tbm/rest/SimpleRESTEJB.java
+++ src/main/java/br/com/tbm/rest/SimpleRESTEJB.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -12,6 +15,14 @@ import javax.ws.rs.Path;
 @Lock(LockType.READ)
 @Path("/ejb")
 public class SimpleRESTEJB {
+	
+	
+	@PersistenceContext(unitName="ardnomweb")
+	EntityManager entityManager;
+	
+	@Inject
+	private UsuarioManager usuarioManager;
+	
     @GET
     public String ejb() {
         return "ejb ok @ " + new Date().toString();
